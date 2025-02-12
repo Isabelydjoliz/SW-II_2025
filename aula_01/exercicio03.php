a4dec2f4d7054aeaa390553b3974c2bf38279566
Isabely D'joliz <124791216+Isabelydjoliz@users.noreply.github.com>
2025-02-12 20:51:34 -0300

    Update exercicio03.php

---
+++ aula_01/exercicio03.php
@@ -0,0 +1,9 @@
+<?php 
+ echo "vamos contas os números ímpares do 0 ao 20" . "<br>" . "<br>";
+for ($numero = 2; $numero <=20; $numero++)
+    if ($numero % 2 == 0)
+    {
+        echo $numero . "<br>";
+    }
+    
+?>
