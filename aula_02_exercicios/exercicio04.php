cdf3cfde822cd1ba808f4be75c80e2a622ddbd68
Isabely D'joliz <124791216+Isabelydjoliz@users.noreply.github.com>
2025-03-10 01:52:32 -0300

    Add files via upload

---
+++ aula_02_exercicios/exercicio04.php
@@ -0,0 +1,17 @@
+<?php
+$opcao = "d";
+switch($opcao){
+    case "a":
+        echo "opção A certa";
+        break;
+    case "b":
+        echo "opção B certa";
+        break;
+    case "c":
+        echo "opção C certa";
+        break;
+    default:
+        echo "opção invalida";
+        break;
+}
+?>
---
+++ aula_02_exercicios/exercicio07.php
@@ -0,0 +1,7 @@
+<?php
+    $vetor = ['Carolina', 'Camila', 'Isabely', 'Letícia', 'Luana', 'Raphaela'];
+
+    foreach ($vetor as $nome) {
+        echo "O seu nome é: " . $nome . "<br>";
+    }
+?>
